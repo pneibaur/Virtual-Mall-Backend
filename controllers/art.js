1885d7827ef7c80669c8050687a7dd5d3270eb8c
DeMarcus Brown <demarcus.m.brown@gmail.com>
2022-09-30 15:15:45 -0400

    added controllers

---
+++ controllers/art.js
@@ -0,0 +1,10 @@
+// DEPENDENCIES
+const express = require('express');
+const { findById } = require('../models/product.js');
+const router = express.Router()
+const Product = require('../models/product.js');
+
+
+
+
+module.exports = router
--- server.js
+++ server.js
@@ -46,6 +46,18 @@ app.get("/", (req, res) => {
   res.send("Hello Virtual Mall!");
 });
 
+const artController = require('./controllers/art.js');
+app.use('/art', artController);
+
+const movieController = require('./controllers/movie.js');
+app.use('/movie', movieController);
+
+const bookController = require('./controllers/book.js');
+app.use('/book', bookController);
+
+const sportController = require('./controllers/sport.js');
+app.use('/sport', sportController);
+
 // -----------------------------------
 // LISTENER
 // ----------------------------------
