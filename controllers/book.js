a64a7989d64bdc62cf7b79b82cfce9ab65e383d3
DeMarcus Brown <demarcus.m.brown@gmail.com>
2022-10-03 10:24:02 -0400

    added book controller

--- controllers/book.js
+++ controllers/book.js
@@ -2,7 +2,53 @@
 const express = require('express');
 const { findById } = require('../models/product.js');
 const router = express.Router()
-const Product = require('../models/product.js');
+const Book = require('../models/product.js');
+
+// Book Index Route
+router.get("/", async (req, res) => {
+    try{
+        //send all books
+        res.json(await Book.find({}));
+    } catch (error) {
+        //send error
+        res.status(400).json(error);
+    }
+})
+
+// Book Create Route
+router.post("/", async (req, res) => {
+    try{
+        //send all books
+        res.json(await Book.create(req.body));
+    } catch (error) {
+        // send error
+        res.status(400).json(error);
+    }
+})
+
+// Book Delete Route
+router.delete("/:id", async (req,res)=> {
+    try{
+        // send all books
+        res.json(await Book.findByIdAndRemove(req.params.id));
+    } catch (error) {
+        // send error
+        res.status(400).json(error)
+    }
+})
+
+// Book Update Route
+router.put("/:id", async (req, res) => {
+    try {
+      // send all books
+      res.json(
+        await Book.findByIdAndUpdate(req.params.id, req.body, { new: true })
+      );
+    } catch (error) {
+      // send error
+      res.status(400).json(error);
+    }
+  });
 
 
 
