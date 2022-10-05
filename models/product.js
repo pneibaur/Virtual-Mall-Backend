778babd9fb18cd1dc5dfde57fff9e502ed287022
DeMarcus Brown <demarcus.m.brown@gmail.com>
2022-10-05 10:20:18 -0400

    adding project image to model

--- models/product.js
+++ models/product.js
@@ -3,6 +3,7 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema({
     storeName: {type: Schema.Types.ObjectId, ref: 'Store'},
     productName: String,
+    productImage: String,
     creator: String,
     price: Number,
     qty: Number,
